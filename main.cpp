57ede6b726652015e36517191d190fecd1e8c5e4
VadimasKo <kozemiakinas123@gmail.com>
2021-02-05 19:09:38 +0200

    Add gender funkction

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include <string>
 
+std::string getGender( std::string name){    //nustato vardo lyti ir pakeicia zinute
+    char last = name.back();
+    std::string message;
+    if(last == 'a' || last=='A'){
+        message = "Sveika, ";
+        message += name;
+    }
+    else{
+        message = "Sveikas, ";
+        message += name;
+    }
 
-int main()
-{
-    std::string message = "Sveiki, ";      
+    return message;
+}
+
+int main(){
+        
     std::string name;
     
     std::cout<<"Iveskite savo varda "<<std::endl;
     std::cin>>name;
     std::cout<<std::endl<<std::endl;
    
-    message += name;
+    std::string message = getGender(name);
+
 
     int length = message.length();
    
