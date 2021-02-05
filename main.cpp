49cfeb8b542f4653b169d39c25cb38c695863382
VadimasKo <kozemiakinas123@gmail.com>
2021-02-05 17:37:14 +0200

    cleaning

--- main.cpp
+++ main.cpp
@@ -8,29 +8,26 @@ using namespace std;
 
 int main()
 {
-    string message = "Sveiki, ";       // main line with message
-
+    string message = "Sveiki, ";      
     string name;
+    
     cout<<"Iveskite savo varda "<<endl;
     cin>>name;
     cout<<endl<<endl;
    
     message += name;
-    string wall(1,'*');
 
     int length = message.length();
    
     string line(length +4,'*');
     string space(length,' ');
+
+    //add walls  
     space.insert(0,"* ");
     space.append(" *");
-    
     message.insert(0,"* ");
     message.append(" *");
 
- 
-    // string line = makeLine('*', message.length() + 2);          // *****
-   // *   *
 
     cout<<line<<endl;
     cout<<space<<endl;
