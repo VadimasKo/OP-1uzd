957180b0cea3cd4dbb3fa10879817688e2e781aa
VadimasKo <kozemiakinas123@gmail.com>
2021-02-06 23:05:03 +0200

    make more robust

--- main.cpp
+++ main.cpp
@@ -57,94 +57,64 @@ class greetingCard{
 
 };
 
-// void getGender( std::string name, std::string * message){    //nustato vardo lyti ir pakeicia zinute
-//     char last = name.back();
-//     if(last == 'a' || last=='A'){
-//         *message = "Sveika, ";
-//         *message += name;
-//     }
-//     else{
-//         *message = "Sveikas, ";
-//         *message += name;
-//     }
-
-// }
-
-// std::string setWidth( std::string message, int length){ //nenaudoju pointerio nes kylo problemu su metodo naudojimu (tikriausiai kadangi string yra char[])
-
-//     std::string upperSpace( length%2 + length/2, ' ');
-//     std::string lowerSpace (length/2, ' ');
-    
-//     upperSpace += message + lowerSpace;
-    
-//     message = upperSpace;
-    
-
-//     return message;
-//     }
 
 int main(){  
 
-    // std::string name;
-    // std::string message;
-    // int length;
-    // char ats = 'y';
+   
 
     std::string name;
     std::cout<<"Iveskite savo varda "<<std::endl;
     std::cin>>name;
+
+
+    //clean cin buffer
+    std::cin.clear();
+    std::cin.ignore(10000, '\n');   
+
     greetingCard card(name);
 
     char ats;
-    std::cout<<"Ar norite pakeisti remelio dydi (y/n)"<<std::endl;
-    std::cin>>ats;
-       if (ats == 'y'){
+    while(1){
+        std::cout<<"Ar norite pakeisti remelio dydi (y/n)"<<std::endl;
+        std::cin>>ats;
+        if (ats == 'y'){
 
-        int length;
-        std::cout<<"iveskite nauja ilgi"<<std::endl;
-        std::cin>>length;
+            int length;
+        
+            while(1){
+                std::cout<<"iveskite nauja ilgi (jeigu nenoryte iveskite '0') "<<std::endl;
+                std::cin>>length;
 
-       card.changeWidth(length - card.message.length());
+                if( length>=card.message.length()){
+                     card.changeWidth(length - card.message.length());
+                     break;
+                }
+                else if(length==0){
+                    break;
+                }
+                else{
+                    std::cout<<"ilgis per mazas arba ivestis netinkama "<<std::endl;
+
+                    std::cin.clear();
+                    std::cin.ignore(10000, '\n');   
+        
+                }
+
+            
+            }
+            break;
+        }
+        else if(ats =='n'){
+            break;
+        }
+
+        //clean cin buffer
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
     }
+   
     
     card.makeLines();
     card.draw();
 
-
-
-
-
-
-
-
-
-
-
-    // std::cout<<std::endl<<std::endl;
-    // getGender(name, &message);
-
-    // 
-
-    // length = message.length();
-
-   
-    // std::cout<<length;
-
-
-   
-    // std::string line(length +4,'*');
-    // std::string space(length,' ');
-
-    // //add walls  
-    // space.insert(0,"* ");
-    // space.append(" *");
-    // message.insert(0,"* ");
-    // message.append(" *");
-
-
-    // std::cout<<line<<std::endl;
-    // std::cout<<space<<std::endl;
-    // std::cout<<message<<std::endl;
-    // std::cout<<space<<std::endl;
-    // std::cout<<line<<std::endl;
 }
